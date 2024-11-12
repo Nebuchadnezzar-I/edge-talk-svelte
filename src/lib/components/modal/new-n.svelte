035db3234ea6e971ebf7faf3813e1a91cb3164bd
Nebuchadnezzar-I <onur.orrmenn@gmail.com>
2024-11-12 09:45:14 +0100

    Mode + Modal

---
+++ src/lib/components/modal/new-n.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { Button } from "$lib/components/ui/button/index.js";
+import { X } from "lucide-svelte";
+let { isExpanded } = $props();
+</script>
+
+<style>
+.modal {
+    transition: all 200ms ease-in-out;
+    z-index: 1000;
+    position: fixed;
+    width: 100%;
+    left: 0;
+    bottom: 0;
+    overflow: hidden;
+}
+</style>
+
+<div
+    class="modal bg-background flex flex-col gap-3 p-3"
+    style="height: {isExpanded ? '100dvh' : '0dvh'};"
+>
+    <div class="flex items-center justify-between">
+        <h3 class="text-md font-semibold">New negotiation</h3>
+        <Button variant="ghost" onclick={() => isExpanded = !isExpanded}>
+            <X />
+        </Button>
+    </div>
+
+    <div class="w-full h-full">
+        Content
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+import * as Avatar from "$lib/components/ui/avatar/index.js";
+import { Button } from "$lib/components/ui/button/index.js";
+import NewNegotiation from '$lib/components/modal/new-n.svelte';
+
+let isExpanded = $state(false);
+</script>
+
+<div class="w-full h-full p-3 flex flex-col gap-3">
+    <!-- Header -->
+    <div class="flex items-center justify-between">
+        <h3 class="text-xl font-semibold">EdgeTalk.ai</h3>
+        <Avatar.Root>
+            <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+            <Avatar.Fallback>CN</Avatar.Fallback>
+        </Avatar.Root>
+    </div>
+
+    <div class="body-content w-full h-full flex items-center justify-center">
+        <Button class="rounded-full px-6" onclick={() => isExpanded = !isExpanded}>
+            New Negotiation
+        </Button>
+    </div>
+
+    <NewNegotiation {isExpanded} />
+</div>
